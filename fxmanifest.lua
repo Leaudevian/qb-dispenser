d8253bd943fc483548ea41540178f174debfb3f9
leaudevian <mastersogood@gmail.com>
2021-09-07 10:37:07 +0200

    fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Dispenser'
+version '1.0.0'
+
+shared_scripts {
+  '@qb-core/import.lua'
+  'config.lua'
+}
+
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+
+dependency 'qtarget'
